7904214ed498f2cbaa80b135f04fa8fdc82b29a5
Masato <me@kokuraden.com>
2023-03-26 17:14:27 +0800

    initial commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace tinynotepad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.text_area = new System.Windows.Forms.TextBox();
+            this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem5 = new System.Windows.Forms.MenuItem();
+            this.menuItem6 = new System.Windows.Forms.MenuItem();
+            this.menuItem7 = new System.Windows.Forms.MenuItem();
+            this.menuItem9 = new System.Windows.Forms.MenuItem();
+            this.menuItem8 = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.menuItem3 = new System.Windows.Forms.MenuItem();
+            this.menuItem4 = new System.Windows.Forms.MenuItem();
+            this.SuspendLayout();
+            // 
+            // text_area
+            // 
+            this.text_area.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.text_area.Font = new System.Drawing.Font("細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.text_area.Location = new System.Drawing.Point(0, 0);
+            this.text_area.Multiline = true;
+            this.text_area.Name = "text_area";
+            this.text_area.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.text_area.Size = new System.Drawing.Size(784, 562);
+            this.text_area.TabIndex = 1;
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1,
+            this.menuItem2});
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Index = 0;
+            this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem5,
+            this.menuItem6,
+            this.menuItem7,
+            this.menuItem9,
+            this.menuItem8});
+            this.menuItem1.Text = "檔案(&F)";
+            // 
+            // menuItem5
+            // 
+            this.menuItem5.Index = 0;
+            this.menuItem5.Text = "開新檔案(&N)";
+            this.menuItem5.Click += new System.EventHandler(this.menuItem5_Click);
+            // 
+            // menuItem6
+            // 
+            this.menuItem6.Index = 1;
+            this.menuItem6.Text = "開啟舊檔(&O)";
+            this.menuItem6.Click += new System.EventHandler(this.menuItem6_Click);
+            // 
+            // menuItem7
+            // 
+            this.menuItem7.Index = 2;
+            this.menuItem7.Text = "儲存檔案(&S)";
+            this.menuItem7.Click += new System.EventHandler(this.menuItem7_Click);
+            // 
+            // menuItem9
+            // 
+            this.menuItem9.Index = 3;
+            this.menuItem9.Text = "-";
+            // 
+            // menuItem8
+            // 
+            this.menuItem8.Index = 4;
+            this.menuItem8.Text = "結束(&X)";
+            this.menuItem8.Click += new System.EventHandler(this.menuItem8_Click);
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Index = 1;
+            this.menuItem2.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem3,
+            this.menuItem4});
+            this.menuItem2.Text = "說明(&H)";
+            // 
+            // menuItem3
+            // 
+            this.menuItem3.Index = 0;
+            this.menuItem3.Text = "說明(&H)";
+            this.menuItem3.Click += new System.EventHandler(this.menuItem3_Click);
+            // 
+            // menuItem4
+            // 
+            this.menuItem4.Index = 1;
+            this.menuItem4.Text = "關於(&A)";
+            this.menuItem4.Click += new System.EventHandler(this.menuItem4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.text_area);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "KokuraPad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox text_area;
+        private System.Windows.Forms.MainMenu mainMenu1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem5;
+        private System.Windows.Forms.MenuItem menuItem6;
+        private System.Windows.Forms.MenuItem menuItem7;
+        private System.Windows.Forms.MenuItem menuItem9;
+        private System.Windows.Forms.MenuItem menuItem8;
+        private System.Windows.Forms.MenuItem menuItem2;
+        private System.Windows.Forms.MenuItem menuItem3;
+        private System.Windows.Forms.MenuItem menuItem4;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+namespace tinynotepad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            text_area.Text = "";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select file";
+            dialog.InitialDirectory = ".\\";
+            dialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                text_area.Text = "";
+
+                StreamReader sr = new StreamReader(dialog.FileName, Encoding.Default, true);
+
+                string line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    text_area.AppendText(line+Environment.NewLine);
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savedialog = new SaveFileDialog();
+            savedialog.Title = "Save file";
+            savedialog.InitialDirectory = ".\\";
+            savedialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (savedialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(savedialog.FileName, text_area.Text);
+            }
+        }
+
+        private void menuItem5_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("是否要儲存變更", "是否儲存", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                SaveFileDialog savedialog = new SaveFileDialog();
+                savedialog.Title = "Save file";
+                savedialog.InitialDirectory = ".\\";
+                savedialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                if (savedialog.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(savedialog.FileName, text_area.Text);
+                }
+                text_area.Text = "";
+            }
+            else
+            {
+                text_area.Text = "";
+            }
+        }
+
+        private void menuItem6_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select file";
+            dialog.InitialDirectory = ".\\";
+            dialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                text_area.Text = "";
+
+                StreamReader sr = new StreamReader(dialog.FileName, Encoding.Default, true);
+
+                string line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    text_area.AppendText(line + Environment.NewLine);
+                }
+            }
+        }
+
+        private void menuItem7_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savedialog = new SaveFileDialog();
+            savedialog.Title = "Save file";
+            savedialog.InitialDirectory = ".\\";
+            savedialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (savedialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(savedialog.FileName, text_area.Text);
+            }
+        }
+
+        private void menuItem8_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void menuItem3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("沒有說明","沒有說明",MessageBoxButtons.OK,MessageBoxIcon.Error);
+        }
+
+        private void menuItem4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("KokuraPad\n版本 0.1\nPowered by Kokuraden.", "關於KokuraPad", MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+    }
+}
